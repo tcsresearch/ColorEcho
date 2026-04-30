578f20f97b8b56e9bf8be90dfffd6aa2e2a4d8eb
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-29 23:49:33 -0500

    Fix quoting for sourced file in AdvancedLoader function
    Enclose sourced file variable in quotes to handle spaces.

--- functions/Cecho_AdvancedLoader.sh
+++ functions/Cecho_AdvancedLoader.sh
@@ -4,7 +4,7 @@ function AdvancedLoader() {
   # Assuming 'files.list' contains a list of filenames, one per line
   for file in $(cat Cecho_functions.list); do
     echo "Loading Function: $file"
-    source $file
+    source "$file"
     if [ $? -eq 0 ] 
     then 
       echo "Successfully Loaded Function: $file." 
